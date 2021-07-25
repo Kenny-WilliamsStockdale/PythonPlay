4c8896dea6836213b083d55c0be33941638e75f6
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2021-07-25 18:44:13 +1200

    prac code

--- Hello.py
+++ Hello.py
@@ -1,2 +1,23 @@
 msg = 'Hello world'
 print(msg)
+
+import random
+
+r1 = ["Gary",
+        "Jim",
+        "Mike",
+        "Dave"]
+
+r2 = ["Code.",
+          "Hike.",
+          "play video games.",
+          "listen to music."]
+
+random_r1 = random.choice(r1)
+random_r2 = random.choice(r2)
+#random_person2 = random.choice(people)
+
+print(f"Hello my name is {random_r1} and I like to {random_r2}")
+
+print("Winter is coming")
+print("You know nothing.")
