220d694b23ce298fef4c2949a25224f976e562b2
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2021-07-26 10:11:14 +1200

    more code examples

--- Hello.py
+++ Hello.py
@@ -1,23 +1,40 @@
-msg = 'Hello world'
-print(msg)
+# msg = 'Hello world'
+# print(msg)
 
-import random
+# import random
 
-r1 = ["Gary",
-        "Jim",
-        "Mike",
-        "Dave"]
+# r1 = ["Gary",
+#         "Jim",
+#         "Mike",
+#         "Dave"]
 
-r2 = ["Code.",
-          "Hike.",
-          "play video games.",
-          "listen to music."]
+# r2 = ["Code.",
+#           "Hike.",
+#           "play video games.",
+#           "listen to music."]
 
-random_r1 = random.choice(r1)
-random_r2 = random.choice(r2)
-#random_person2 = random.choice(people)
+# random_r1 = random.choice(r1)
+# random_r2 = random.choice(r2)
+# #random_person2 = random.choice(people)
 
-print(f"Hello my name is {random_r1} and I like to {random_r2}")
+# print(f"Hello my name is {random_r1} and I like to {random_r2}")
 
-print("Winter is coming")
-print("You know nothing.")
+# print("Winter is coming")
+# print("You know nothing.")
+
+# a_list = []
+# a_tuple = (1, "one")
+# a_list = a_list + [a_tuple]
+
+# print(a_list)
+
+# a_number = 1
+# b_number = 2
+# # swap 
+# b_number, a_number = a_number, b_number
+# print(a_number, b_number)
+
+# """
+# This is a test
+
+# """
