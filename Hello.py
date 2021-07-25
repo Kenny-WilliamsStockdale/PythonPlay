080326f4e14161e98bd6b8d5fb1acc32f2c4ec52
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2021-07-26 10:59:44 +1200

    added fizz buzz activity

--- Hello.py
+++ Hello.py
@@ -38,3 +38,14 @@
 # This is a test
 
 # """
+for i in range(19):
+    if i == 0:
+        print(i)
+    elif i % 3 == 0 and i % 5 == 0:
+        print('fizzBuzz')
+    elif i % 3 == 0:
+                print('fizz')
+    elif i % 5 == 0:
+            print('buzz')
+    else: print(i)
+        
