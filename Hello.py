0874a82f630c4bced3442f300579088e123a3dc3
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2021-07-26 15:22:36 +1200

    added fizz buzz activity

--- Hello.py
+++ Hello.py
@@ -34,18 +34,85 @@
 # b_number, a_number = a_number, b_number
 # print(a_number, b_number)
 
-# """
-# This is a test
+# fizz_buzz activity
 
-# """
-for i in range(19):
-    if i == 0:
-        print(i)
-    elif i % 3 == 0 and i % 5 == 0:
-        print('fizzBuzz')
-    elif i % 3 == 0:
-                print('fizz')
-    elif i % 5 == 0:
-            print('buzz')
-    else: print(i)
+#part 1
+# def fizz_buzz():
+#       print(0)
+#       for i in range(1, 16):
+#           if i % 3 == 0 and i % 5 == 0:
+#             print('fizzBuzz')
+#           elif i % 3 == 0:
+#             print('fizz')
+#           elif i % 5 == 0:
+#             print('buzz')
+#           else:
+#             print(i)
+# fizz_buzz()
+
+# part 2
+# def fizz_buzz(i = 15):
+#       x = 1
+#       print(0)
+#       while x <= i:
+#         if x % 3 == 0 and x % 5 == 0:
+#             print('fizzBuzz')
+#         elif x % 3 == 0:
+#             print('fizz')
+#         elif x % 5 == 0:
+#             print('buzz')
+#         else:
+#             print(x)
+#         x += 1    
+# fizz_buzz()
+
+#part 3 
+# def fizz_buzz(i = 15):
+#         x = 0
+#         list = []
+#         list.append(tuple((x, str(x))))
+#         while x < i + 1:
+#                 # print(x) 
+#             if x % 3 == 0 and x % 5 == 0:
+#                 list.append(tuple((x,'fizzBuzz')))
+#                 # print('fizzBuzz')
+#             elif x % 3 == 0:
+#                 list.append(tuple((x,'fizz')))
+#                 # print('fizz')
+#             elif x % 5 == 0:
+#                 list.append(tuple((x,'buzz')))
+#                 # print('buzz')
+#             else:
+#                 list.append(tuple((x, str(x)))) 
+#                 # print(x)
+#             x += 1
+#         print(list)
+# fizz_buzz()
+
+# def fizz_buzz(fizz=3, buzz= 5, up_to_number= 15):
+#         x = 0
+#         result = []
+#         while x < up_to_number + 1:
+#             if x % 3 == 0 and x % 5 == 0:
+            
+#             elif x % 3 == 0:
+                
+#             elif x % 5 == 0:
         
+#             else:
+def fizz_buzz(fizz=3,buzz=5,up_to=15):
+        x = 1;
+        result =[]
+        while x < up_to:
+                
+                string = ''
+                if x%fizz==0:
+                        string += 'Fizz'
+                if x%buzz==0:
+                        string += 'Buzz'
+                result.append((x,(str(x) if string == "" else string)))
+                x+=1
+        return  result
+print(fizz_buzz(3,5,100))       
+
+# x = count i = up_to_number
