217f1448a6d3685168075a6d60d881df490b9e5d
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2021-08-09 09:03:27 +1200

    first few exercises

---
+++ python-exercises.py
@@ -0,0 +1,27 @@
+# activity 1 - Write a Python program to print the following string in a specific format
+# output:
+# Twinkle, twinkle, little star,
+# 	How I wonder what you are! 
+# 		Up above the world so high,   		
+# 		Like a diamond in the sky. 
+# Twinkle, twinkle, little star, 
+# 	How I wonder what you are
+
+msg = ("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
+print(msg)
+
+# activity 2 - Write a Python program to get the Python version you are using
+import sys
+print("Python version")
+print (sys.version)
+print("Version info.")
+print (sys.version_info)
+
+# activity 3 - Write a Python program to display the current date and time.
+from time import ctime
+ctime()
+print("current date and time:\n" + ctime())
+
+# activity 4 -
+
+ 
